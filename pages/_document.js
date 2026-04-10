2ab5f55edfbef5608ad545c643c1c4b023118ac4
JOAO GABRIEL ALBUQUERQUE ARAUJO <joaogabriel@Macbook-de-Joao-Gabriel.local>
2026-04-09 22:03:30 -0300

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#020208" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </Head>
+      <body style={{margin: 0, padding: 0, background: '#020208', fontFamily: "'Inter', sans-serif"}}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
